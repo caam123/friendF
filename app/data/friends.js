e7111dba4350959c184286d516a0709e97f6ac3b
Charlie <caam_1275@hotmail.com>
2019-06-10 00:20:03 -0500

    Architecture of my folders was fixed - Created apiRoutes and server js correctly referenced to the required files

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+
+ var friendArray = [
+  {
+    name:"Charles Lee Ray AKA Chucky",
+    photo:"https://en.wikipedia.org/wiki/Chucky_(character)#/media/File:Chucky_(Child's_Play).jpg",
+    scores:[
+        1,
+        2,
+        5,
+        1,
+        4,
+        2
+      ]
+  },
+
+{
+    name:"Frederick Charles Krueger",
+    photo:"https://en.wikipedia.org/wiki/Freddy_Krueger#/media/File:Freddy_Krueger_(Robert_Englund).jpg",
+    scores:[
+        2,
+        1,
+        2,
+        3,
+        1,
+        4
+      ]
+  },
+  
+  {
+    name:"Jason Voorhes",
+    photo:"https://es.wikipedia.org/wiki/Jason_Voorhees#/media/Archivo:WW_Chicago_2014_Contest_-_Jason_Voorhees_(14881804197).jpg",
+    scores:[
+        5,
+        3,
+        3,
+        5,
+        5,
+        3
+      ]
+  },
+  
+  {
+    name:"Michael Myers",
+    photo:"https://es.wikipedia.org/wiki/Michael_Myers#/media/Archivo:Wondercon_2015-8026_(16876251440).jpg",
+    scores:[
+        5,
+        5,
+        3,
+        2,
+        3,
+        1
+      ]
+  }
+  
+];  
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,27 @@
+//LOAD DATA
+//Esto linkea los arrays de DATA, en este caso los arrays de amigos que existen por default hasta el momento, y despues los que se vayan agregando
+// Para linkearlos/agregarlos/importarlos usamos "require"
+
+var friendsData = require("../data/friends")
+
+// ROUTING
+
+module.exports = function(app){
+    //Esta seccion maneja que pasa cuando el usuario accede a cierta ruta, se le muestra un archivo JSON ¿?
+    //Usamos GET porque obtenemos la info de algún otro lugar
+
+    //GET
+
+    app.get("/api/friends", function(req, res){
+        res.json(friendsData)
+    });
+
+    // POSTS
+
+    app.post("api/friends", function(req, res){
+        friendsData.push(req.body);
+        res.json(true);
+
+    });
+
+}
